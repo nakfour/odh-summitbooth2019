4314313aeff662320d156f9fb532636fcbc4f0f0
Juana Nakfour <nakfour>
2019-04-22 19:27:30 -0500

    Added model metrics

--- seldon-model/mymodel.py
+++ seldon-model/mymodel.py
@@ -7,9 +7,20 @@ import json
 
 
 class mymodel(object):
- 
+    
     def __init__(self):
         print("Initializing")
+        # Variables needed for metric collection
+        self.V3=0
+        self.V4=0
+        self.V10=0
+        self.V11=0
+        self.V12=0
+        self.V14=0
+        self.V17=0
+        self.Amount=0
+        self.proba_1 = 0
+        
         # Create an S3 client
         s3 = boto3.client(service_name='s3',aws_access_key_id='foo', aws_secret_access_key='bar', endpoint_url='http://ceph:8000')
         key = "uploaded/model.pkl"
@@ -31,17 +42,26 @@ class mymodel(object):
         #self.model = 'None'
  
     def predict(self,x,features_names):
-        print("HERERERERERERERERERERERERE")
         print(x)
         print(type(x))
         result = "PASS"
         featurearray=[float(i) for i in x.split(',')]
+        # grabbing features for metric to be scraped by prometheus
+        self.V3 = featurearray[0]
+        self.V4 = featurearray[1]
+        self.V10 = featurearray[2]
+        self.V11 = featurearray[3]
+        self.V12 = featurearray[4]
+        self.V14 = featurearray[5]
+        self.V17 = featurearray[6]
+        self.Amount = featurearray[7]
+        
         print(featurearray)
         rowdf = pd.DataFrame([featurearray], columns = ['V3','V4','V10','V11','V12','V14','V17','Amount'])
         print(rowdf)
+        self.proba_1 = self.clf.predict_proba(rowdf)[:,1]
+        
         predictions = self.clf.predict(rowdf)
-        #predictions = self.clf.predict_proba()
-        # initialize list of lists
         return predictions
         
     def metrics(self):
@@ -49,4 +69,13 @@ class mymodel(object):
             {"type":"COUNTER","key":"mycounter","value":1}, # a counter which will increase by the given value
             {"type":"GAUGE","key":"mygauge","value":100}, # a gauge which will be set to given value
             {"type":"TIMER","key":"mytimer","value":20.2}, # a timer which will add sum and count metrics - assumed millisecs
+            {"type":"GAUGE","key":"V3","value":self.V3},
+            {"type":"GAUGE","key":"V4","value":self.V4},
+            {"type":"GAUGE","key":"V10","value":self.V10},
+            {"type":"GAUGE","key":"V11","value":self.V11},
+            {"type":"GAUGE","key":"V12","value":self.V12},
+            {"type":"GAUGE","key":"V14","value":self.V14},
+            {"type":"GAUGE","key":"V17","value":self.V17},
+            {"type":"GAUGE","key":"Amount","value":self.Amount},
+            {"type":"GAUGE","key":"proba_1","value":self.proba_1[0]},
             ]
